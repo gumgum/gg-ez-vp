9654a2cf9ea1071b7cad6dec8aa2edb4cc4ed24e
Luiz M <lmendez@gumgum.com>
2019-07-10 14:36:45 -0500

    main.js file created

---
+++ src/main.js
@@ -0,0 +1,17 @@
+export default class GGezvp {
+  constructor(setup) {
+    this.config = {
+      container,
+      width,
+      height,
+      src,
+      controls,
+      autoPlay,
+      volume,
+      mute,
+      poster,
+      preload,
+      loop
+    } = setup;
+  }
+}

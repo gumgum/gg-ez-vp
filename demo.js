e8a3efd6b900633f925ef2b09e122ab0694ebf6f
Eder Sanchez <eder@gumgum.com>
2019-07-12 11:00:47 -0500

    show two instances on demo page

--- demo.js
+++ demo.js
@@ -4,19 +4,10 @@ window.onload = function onload() {
     console.log({ GgEzVp });
 
     // MP4 test
-    //let playerInstance = new GgEzVp({
-    //    container: 'videoContainer1',
-    //    src: 'https://aba.gumgum.com/13861/8/big_buck_bunny_640x360.mp4',
-    //    autoplay: false,
-    //});
-
-    // VAST test
     let playerInstance = new GgEzVp({
         container: 'videoContainer1',
-        src:
-            'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=[timestamp]',
-        autoplay: false,
-        isVAST: true
+        src: 'https://aba.gumgum.com/13861/8/big_buck_bunny_640x360.mp4',
+        autoplay: true
     });
 
     // Configure player
@@ -43,4 +34,38 @@ window.onload = function onload() {
         });
     });
     playerInstance.on('error', console.log);
+
+    // VAST test
+    let playerInstance1 = new GgEzVp({
+        container: 'videoContainer2',
+        src:
+            'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=[timestamp]',
+        autoplay: false,
+        isVAST: true
+    });
+
+    // Configure player
+    const playBtn1 = document.getElementById('play-btn1');
+    playBtn1.addEventListener('click', () => playerInstance1.playPause());
+
+    const muteBtn1 = document.getElementById('mute-btn1');
+    muteBtn1.addEventListener('click', () => playerInstance1.muteUnmute());
+
+    const rmBtn1 = document.getElementById('destroy-btn1');
+    rmBtn1.addEventListener('click', () => playerInstance1.destroy());
+
+    const f5Btn1 = document.getElementById('reload-btn1');
+    f5Btn1.addEventListener('click', () => location.reload());
+
+    // Set listeners
+    playerInstance1.on('ready', () => {
+        playerInstance1.on('play', console.log);
+        playerInstance1.on('pause', console.log);
+        playerInstance1.on('timeupdate', console.log);
+        playerInstance1.on('predestroy', () => {
+            console.log('PRE DESTROY LISTENER');
+            playerInstance1 = null;
+        });
+    });
+    playerInstance1.on('error', console.log);
 };

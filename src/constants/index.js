ad82caa2ef308d45d4e84d39e19d85f1db33ea87
Eder Sanchez <eder@gumgum.com>
2019-10-17 18:44:48 -0500

    move defaultOptions to constants file

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,3 +4,45 @@ export const PLAYER_CLICK = 'player-click';
 export const PRE_DESTROY = 'pre-destroy';
 export const READY = 'ready';
 export const RESIZE = 'resize';
+export const DEFAULT_OPTIONS = {
+    container: null,
+    width: null,
+    height: null,
+    src: null,
+    controls: {
+        bg: null,
+        color: '#FFFFFF',
+        play: {
+            color: null,
+            src: null
+        },
+        stop: {
+            color: null,
+            src: null
+        },
+        replay: {
+            color: null,
+            src: null
+        },
+        volume: {
+            color: null,
+            src: null
+        },
+        fullscreen: {
+            color: null,
+            src: null
+        },
+        timer: {
+            color: null
+        }
+    },
+    autoPlay: false,
+    volume: 1,
+    muted: true,
+    poster: null,
+    preload: 'auto',
+    loop: false,
+    isVAST: false,
+    fullscreen: false,
+    playsinline: true
+};

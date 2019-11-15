d1fc9cce498c5b51342c473a9ebb0df1888b9915
edrpls <edr@edrpls.com>
2019-11-14 20:42:20 -0600

    add skip component

--- src/lib/controls/skip.js
+++ src/lib/controls/skip.js
@@ -1,3 +1,20 @@
+import createNode from '../../helpers/createNode';
+import { SKIP } from '../../constants';
+
+const nextTick = requestAnimationFrame || setTimeout;
+
 export default function skip(container) {
-    console.log('skip');
+    const button = createNode('button', this.__getCSSClass('button-skip'), {
+        type: 'button'
+    });
+    button.innerText = SKIP;
+
+    this.__nodeOn(button, 'click', e => {
+        e.stopPropagation?.();
+        this.emitter.emit(SKIP);
+        nextTick(this.destroy);
+    });
+    // TODO: call skippable methods
+    this.container.classList.add(this.__getCSSClass(SKIP));
+    container.appendChild(button);
 }

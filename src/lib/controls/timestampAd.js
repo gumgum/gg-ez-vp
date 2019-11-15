6b7c593bf062de82624bea41a292fa9ba1fc0aa2
edrpls <edr@edrpls.com>
2019-11-14 20:42:35 -0600

    add timestampAd component

--- src/lib/controls/timestampAd.js
+++ src/lib/controls/timestampAd.js
@@ -1,3 +1,34 @@
+import { PLAYBACK_PROGRESS, TIMESTAMP } from '../../constants';
+import secondsToReadableTime from '../../helpers/secondsToReadableTime';
+import createNode from '../../helpers/createNode';
+
 export default function timestampAd(container) {
-    console.log('timestampAd');
+    const getRemainingTime = () =>
+        this.isVPAID
+            ? this.VPAIDWrapper.getAdRemainingTime()
+            : this.player.duration - this.player.currentTime;
+
+    const initialRemainingTime = getRemainingTime();
+
+    const timestampNode = createNode('div', `${this.__getCSSClass(TIMESTAMP)}-ad`);
+    const nodeUpdater = setNodeText(timestampNode);
+    nodeUpdater(initialRemainingTime);
+
+    // Set duration once playback starts if it wasn't available
+    this.once('loadedmetadata', () => {
+        const remainingTime = getRemainingTime();
+        if (!initialRemainingTime) nodeUpdater(remainingTime);
+    });
+
+    //TODO: Throttle DOM updates when fancyCurrentTime is the same
+    // Update currentTime
+    this.on(PLAYBACK_PROGRESS, ({ remainingTime }) => {
+        nodeUpdater(remainingTime);
+    });
+
+    container.appendChild(timestampNode);
 }
+
+const setNodeText = node => remainingTime => {
+    node.innerText = `Ad ${Math.floor(remainingTime)}`;
+};

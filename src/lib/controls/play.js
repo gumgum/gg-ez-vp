da8d2133c5b9b18f390d5090961520a02b2a312b
edrpls <edr@edrpls.com>
2019-11-20 20:16:46 -0600

    preload volume/play state icons

--- src/lib/controls/play.js
+++ src/lib/controls/play.js
@@ -1,4 +1,5 @@
 import createNode from '../../helpers/createNode';
+import preloadIcons from '../../helpers/preloadIcons';
 import { PLAY } from '../../constants';
 const PAUSE = 'pause';
 const REPLAY = 'replay';
@@ -7,6 +8,9 @@ export default function play(container) {
     const button = createNode('button', [this.__getCSSClass('button-icon'), PLAY], {
         type: 'button'
     });
+    const inactiveIcons = [REPLAY, PAUSE];
+
+    preloadIcons(inactiveIcons, this.__baseURL);
 
     this.__nodeOn(button, 'click', e => {
         e.stopPropagation?.();
@@ -14,7 +18,7 @@ export default function play(container) {
     });
 
     this.on(PLAY, listenerCreator(button, [REPLAY, PLAY], PAUSE));
-    this.on(PAUSE, listenerCreator(button, [REPLAY, PAUSE], PLAY));
+    this.on(PAUSE, listenerCreator(button, inactiveIcons, PLAY));
     this.on('ended', listenerCreator(button, [PLAY, PAUSE], REPLAY));
 
     container.appendChild(button);

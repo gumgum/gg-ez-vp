d61ce78948ba16f9d508674658bed2e5f77fecc0
Estavillo <fernando@gumgum.com>
2020-05-26 04:13:07 -0700

    Add nonlinear creative support

--- src/helpers/parseVAST.js
+++ src/helpers/parseVAST.js
@@ -1,8 +1,8 @@
-import { VASTClient } from 'vast-client';
 import { DEFAULT_VAST_OPTIONS } from '../constants';
+import { VASTClient } from 'vast-client';
+import addOverlays from '../helpers/addOverlays';
 import mediaFilesReducer from '../helpers/mediaFilesReducer';
 import replaceVASTMacros from '../helpers/replaceVASTMacros';
-import addOverlays from '../helpers/addOverlays';
 
 // Wrapper for VAST-Client.get()
 export default async function parseVAST(src, options = DEFAULT_VAST_OPTIONS) {
@@ -13,24 +13,29 @@ export default async function parseVAST(src, options = DEFAULT_VAST_OPTIONS) {
     const parsedVAST = await vastClient.get(srcWithSupportedMacros, options);
     this.VASTData = parsedVAST;
     const ad = parsedVAST?.ads[0];
-    const linearCreative = ad?.creatives?.find(({ type }) => type === 'linear');
-    if (!linearCreative) return;
+    const creative = ad?.creatives[0];
+    if (!creative) return;
 
-    const { mediaFiles = [] } = linearCreative;
-    // Separate any JavaScript files from MediaFiles
-    const sources = mediaFilesReducer(mediaFiles);
-    this.VASTSources = sources;
-    const VPAIDSource = sources.javascript.find(({ apiFramework }) => apiFramework === 'VPAID');
-
+    let VPAIDSource;
+    if (creative.type === 'linear') {
+        const { mediaFiles = [] } = creative;
+        // Separate any JavaScript files from MediaFiles
+        const sources = mediaFilesReducer(mediaFiles);
+        this.VASTSources = sources;
+        VPAIDSource = sources.javascript.find(({ apiFramework }) => apiFramework === 'VPAID');
+    } else if (creative.type === 'nonlinear') {
+        VPAIDSource = {
+            fileURL: creative?.variations[0].staticResource
+        };
+    }
     // Adds ovelrlays to block and handle clicks from mobile
     addOverlays.call(this, !!VPAIDSource);
 
     // Load VPAID and run it
     if (VPAIDSource) {
         this.isVPAID = true;
-        return this.__runVPAID(linearCreative, VPAIDSource, vastClient, ad);
+        return this.__runVPAID(creative, VPAIDSource, vastClient, ad);
     }
-
     // if there is no VPAID, fallback to VAST tracking
-    this.__enableVASTTracking(vastClient, ad, linearCreative);
+    this.__enableVASTTracking(vastClient, ad, creative);
 }
